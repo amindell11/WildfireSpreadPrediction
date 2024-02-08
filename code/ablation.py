b6375878fbff420e50189425b6527e49085536e9
Arthur Qin <49544318+Galagalagalaga@users.noreply.github.com>
2024-02-07 22:55:50 -0800

    kk

--- code/ablation.py
+++ code/ablation.py
@@ -36,7 +36,7 @@ for epoch in parameters.epochs_try:
         "optimizer": "adam",
         "epochs": num_epochs,
         "batch_size": 128,
-        "taken_out": "vs"
+        "taken_out": "tmmn"
         }
     )
     print(f"Starting Training: \
